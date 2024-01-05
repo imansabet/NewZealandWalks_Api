887212181f14ce6eea8ec53af430a02759f6979d
iman <dev.imansabet@gmail.com>
2024-01-05 12:16:14 +0330

    GetAll Region Endpoint

---
+++ NewZealandWalks/Controllers/RegionsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewZealandWalks.Data;
+
+namespace NewZealandWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public RegionsController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() 
+        {
+            var regions = _db.Regions.ToList();
+            return Ok(regions);
+        }
+    }
+}
