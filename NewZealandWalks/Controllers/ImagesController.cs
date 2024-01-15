f636502265bc43075ba273ebf63d8f14c68d1c4d
iman <dev.imansabet@gmail.com>
2024-01-15 20:51:34 +0330

    Create ImageController and add Upload action

---
+++ NewZealandWalks/Controllers/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewZealandWalks.Models.DTOs;
+
+namespace NewZealandWalks.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        //POST:/api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDTO request) 
+        {
+            ValidateFileUpload(request);
+            if (ModelState.IsValid) 
+            {
+                
+            }
+            return BadRequest(ModelState);
+        }
+        private void ValidateFileUpload(ImageUploadRequestDTO request) 
+        {
+            var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowedExtensions.Contains(Path.GetExtension(request.File.FileName))) 
+            {
+                ModelState.AddModelError("file", "Unsupported file Extension");
+            }
+            if (request.File.Length > 10485760) 
+            {
+                ModelState.AddModelError("file", "File Size Should be less than 10Mb");
+            }
+        
+        }
+    }
+
+}
