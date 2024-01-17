e857b66ab349ef51b0938957481afbe6c46d8452
iman <dev.imansabet@gmail.com>
2024-01-17 18:52:54 +0330

    Logging Information To File

--- NewZealandWalks/Program.cs
+++ NewZealandWalks/Program.cs
@@ -15,6 +15,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var logger = new LoggerConfiguration()
     .WriteTo.Console()
+    .WriteTo.File("Logs/NZWalks_Log.txt",rollingInterval:RollingInterval.Day)
     .MinimumLevel.Information()
     .CreateLogger();
 
